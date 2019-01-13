49b09ffacac7158c0b558d03db633c48742393c8
AymericAF <aymeric.filliot@gmail.com>
2019-01-13 19:02:46 +0100

    New

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/", name="user")
      */
     public function index()
     {
